b240ec38e4be26d1f147f59f617f39cc9bd0cc0f
lucas-procopio <lucasprocopio.econ@yahoo.com>
2023-07-10 12:35:37 -0300

    update class to access bucket"s storage

--- data_pipeline_ANAC/access_to_datalake.py
+++ data_pipeline_ANAC/access_to_datalake.py
@@ -19,28 +19,22 @@ class storageAccessObj():
         # Getting object called bucket
         return self.storage_client.get_bucket(self.bucket)
 
-    def bucketListObject(self, fromDatabase:str, filename:str, formatFile:str):
+    def bucketListObject(self, fromDatabase:str, filename:str, formatFile:str, yearPath:str):
         # Getting blobs using some filters like parameters
         list_blobs = self._bucketObject()
         return [blob.name for blob in list_blobs.list_blobs() if fromDatabase.lower() in blob.name 
-                and filename.lower() in blob.name and (formatFile.upper() in blob.name or formatFile.lower() in blob.name)]
+                and filename.lower() in blob.name and (formatFile.upper() in blob.name or formatFile.lower() in blob.name)
+                and yearPath.lower() in blob.name]
 
     def downloadFileStorage(self, storage_path, local_path):
         bucket = self._bucketObject()
         blob = bucket.blob(storage_path)
         return blob.download_to_filename(f'{local_path}')
 
-    def downloadListFiles(self, fromDatabase:str, filename:str, formatFile:str):
-        filesDownloadList = self.bucketListObject(fromDatabase, filename, formatFile)
+    def downloadListFiles(self, fromDatabase:str, filename:str, formatFile:str, yearPath:str):
+        filesDownloadList = self.bucketListObject(fromDatabase, filename, formatFile, yearPath)
         for filePath in filesDownloadList:
             paths = filePath.rsplit('/', 1)
             ifExist = os.path.exists(paths[0])
-            if ifExist is False:
-                os.makedirs(paths[0])
+            os.makedirs(paths[0]) if ifExist is False else None
             self.downloadFileStorage(filePath, os.path.join('./',filePath))
-        
-
-if __name__ == '__main__':
-    objectA = storageAccessObj('datalake_f')
-    objectA.downloadListFiles('anac', 'tarifas_transporte_aereo', '.csv')
-    
