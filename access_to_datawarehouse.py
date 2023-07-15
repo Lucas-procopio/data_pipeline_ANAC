75fa357291057c892bd7ee8eb75ed31a92082a00
lucas-procopio <lucasprocopio.econ@yahoo.com>
2023-07-15 19:19:37 -0300

    add _creatingtable"s method

--- access_to_datawarehouse.py
+++ access_to_datawarehouse.py
@@ -3,13 +3,18 @@ import os
 
 CREDENTIALS_PATH = './credentials/gcp_credentials.json'
 
-class bigqueryAcessObj():
+class bigqueryAccessObj():
     ''' Creating a client bigquery instance.'''
 
     @staticmethod
     def startingClient(path):
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path
-        return bigquery.client()
+        return bigquery.Client()
 
     def __init__(self, bigquery_client=startingClient(CREDENTIALS_PATH)):
         self.bigquery_client = bigquery_client
+
+    def _creatingtable(self, table_id, schema):
+        table = bigquery.Table(table_id, schema=schema)
+        table = self.bigquery_client.create_table(table)
+        print(f'Created table {table.project}.{table.dataset_id}.{table.table_id}')
