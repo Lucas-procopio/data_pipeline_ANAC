98679ec3bdfe7464e43a3629df79e75796064326
lucas-procopio <lucasprocopio.econ@yahoo.com>
2023-07-20 22:42:32 -0300

    update __loaddataontable

--- access_to_datawarehouse.py
+++ access_to_datawarehouse.py
@@ -20,10 +20,12 @@ class bigqueryAccessObj():
         table = self.bigquery_client.create_table(table)
         print(f'Created table {table.project}.{table.dataset_id}.{table.table_id}')
 
-    def __loaddataontable(self):
-        pass
+    def __loaddataontable(self, dataframe, table_id, schema):
+        job_config = bigquery.LoadJobConfig(schema=schema)
+        job = self.bigquery_client.load_table_from_dataframe(dataframe, table_id, job_config) # making api request
+        job.result() # getting job results
 
-    def updatetable(self, table_id, schema):
+    def updatetable(self, dataframe, table_id, schema):
         try:
             print(f'Starting loading data at {table_id}.')
             self.__loaddataontable()
@@ -32,11 +34,11 @@ class bigqueryAccessObj():
             try:
                 print(f"Table {table_id} didn't exist. It'll create first.")
                 print(f'Starting creating {table_id}.')
-                self._creatingtable(table_id, schema)
+                self._creatingtable(table_id, dataframe)
                 print(f'Table {table_id} was created.')
                 sleep(5)
                 print(f'Starting loading data at {table_id} again.')
-                self.__loaddataontable()
+                self.__loaddataontable(dataframe, table_id, schema)
                 print(f'Data was loaded successfully at {table_id}.')
             except:
                 print('This something wrong with parameters passed.')
